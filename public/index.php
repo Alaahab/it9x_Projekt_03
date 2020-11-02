b88e9cc9b565c2a8e6019c93264bd4e272d32c3c
Alaa Haboubi <a.haboubi@atlantismedia.de>
2020-11-02 11:38:40 +0100

    AH-1 Create forum part 1

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+use Application\Database;
+
+require '../application/Autoload.php';
+Application\Autoload::register();
+
+
+if (isset($_GET['p'])) {
+    $p = $_GET['p'];
+} else {
+    $p = 'home';
+}
+
+
+$db = new Database('blog');
+
+ob_start();
+if ($p === 'home') {
+    require '../pages/home.php';
+} elseif ($p === 'article') {
+    require '../pages/single.php';
+}
+
+
+$content = ob_get_clean();
+require '../pages/templates/default.php';
